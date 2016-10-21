0e60880d312389d4d53b26afbf55c0e6aba004e9
drcraig20 <drcraig20@gmail.com>
2016-10-21 21:49:46 +0100

    added web concurrency and initialized mongodb production database

--- server/app.js
+++ server/app.js
@@ -10,27 +10,40 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var throng = require('throng');
+var WORKERS = process.env.WEB_CONCURRENCY || 2;
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-	console.error('MongoDB connection error: ' + err);
-	process.exit(-1);
-	}
-);
+function start(){
+  mongoose.connect(config.mongo.uri, config.mongo.options);
+  mongoose.connection.on('error', function(err) {
+      console.error('MongoDB connection error: ' + err);
+      process.exit(-1);
+    }
+  );
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+  if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
-var app = express();
-var server = require('http').createServer(app);
-require('./config/express')(app);
-require('./routes')(app);
+  var app = express();
+  var server = require('http').createServer(app);
+  require('./config/express')(app);
+  require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+  server.listen(config.port, config.ip, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
 
 // Expose app
-exports = module.exports = app;
+  exports = module.exports = app;
+
+}
+
+console.log('Server currently using: %d WEB Workers!!!', WORKERS);
+
+
+throng({
+  workers: WORKERS,
+  start: start
+});
